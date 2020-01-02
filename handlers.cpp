76cf14eab20f47dc0e92dbdd53dc20631d5da4ba
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2020-01-02 10:18:46 +0330

    editing handlers

--- handlers.cpp
+++ handlers.cpp
@@ -562,6 +562,7 @@ Response *ShowDetailHandler::callback(Request *req)
 	body += "<!DOCTYPE html>";
 	body += "<html>";
 	body += "<body style=\"text-align: center;\">";
+	cout << req->getBodyParam("id") << endl;
 	int id = stoi(req->getBodyParam("id"));
 	string line = "GET productDetail ? productId " + to_string(id);
 	vector <string> query = splitBySpace(line);
